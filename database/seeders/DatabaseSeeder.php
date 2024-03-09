bf73edac67f59342e91da2fe0bb7c54f8818af8c
Oxana1976 <oxana.er@hotmail.com>
2024-03-09 19:32:31 +0100

    Update modèle User +migration +seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,9 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
         $this->call([
             ArtistSeeder::class,
+            UserSeeder::class,
         ]);
+
+       
     }
 }
